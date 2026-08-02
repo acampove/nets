d1d2b68fe40575403d225c934e91abaefe742968
Angel Campoverde <angel.campoverde@cern.ch>
2026-08-02 23:29:53 +0800

    Add network test

--- tests/test_network.rs
+++ tests/test_network.rs
@@ -1,5 +1,6 @@
 use nets::network::Network;
 use std::num::NonZeroUsize;
+use ndarray::array;
 
 #[test]
 fn inizialize() 
@@ -13,4 +14,18 @@ fn inizialize()
     let _network = Network::new(&sizes);
 }
 
+#[test]
+fn forward() 
+{
+    let sizes = [
+        NonZeroUsize::new(3).unwrap(),
+        NonZeroUsize::new(4).unwrap(),
+        NonZeroUsize::new(2).unwrap(),
+    ];
 
+    let network = Network::new(&sizes);
+    let input   = array![[1.0, 2.0, 3.0]];
+    let output  = network.forward(&input);
+
+    assert_eq!(output.shape(), &[1, 2]);
+}
