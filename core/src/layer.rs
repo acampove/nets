f728b1a94a2bde06c8473babfaef9e061dbbb21e
Angel Campoverde <angel.campoverde@cern.ch>
2026-09-06 19:11:30 +0800

    Spaces

--- core/src/layer.rs
+++ core/src/layer.rs
@@ -11,9 +11,9 @@ pub struct Weights
     pub values : Array2<f64>
 }
 
-impl std::fmt::Debug for Weights 
+impl std::fmt::Debug for Weights
 {
-    fn fmt(&self, form: &mut std::fmt::Formatter) -> std::fmt::Result 
+    fn fmt(&self, form: &mut std::fmt::Formatter) -> std::fmt::Result
     {
         let nrows = self.values.nrows();
         let ncols = self.values.ncols();
@@ -27,9 +27,9 @@ pub struct Biases
     pub values : Array2<f64>
 }
 
-impl std::fmt::Debug for Biases 
+impl std::fmt::Debug for Biases
 {
-    fn fmt(&self, form: &mut std::fmt::Formatter) -> std::fmt::Result 
+    fn fmt(&self, form: &mut std::fmt::Formatter) -> std::fmt::Result
     {
         let ncols = self.values.ncols();
 
@@ -37,13 +37,13 @@ impl std::fmt::Debug for Biases
     }
 }
 // ---------------------------
-pub struct Layer 
+pub struct Layer
 {
     pub weights: Weights,
     pub biases : Biases ,
 }
 // ---------------------------
-impl Layer 
+impl Layer
 {
     /// Initializer for layer
     /// Args:
@@ -60,7 +60,7 @@ impl Layer
     ///
     /// let layer = Layer::new(in_size, out_size);
     /// ```
-    pub fn new(input_size: NonZeroUsize, output_size: NonZeroUsize) -> Self 
+    pub fn new(input_size: NonZeroUsize, output_size: NonZeroUsize) -> Self
     {
         let limit        = (1.0 / input_size.get() as f64).sqrt();
         let distribution = Uniform::new(-limit, limit);
