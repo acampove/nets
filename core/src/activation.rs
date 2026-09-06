c4cf44596a3f3d4cc1901cd1115f8538abb8a894
Angel Campoverde <angel.campoverde@cern.ch>
2026-09-06 20:32:32 +0800

    Replace sigmoid function with struc

--- core/src/activation.rs
+++ core/src/activation.rs
@@ -1,8 +1,22 @@
-use ndarray::Array2;
+use::ndarray::Array2;
 
-/// Sigmoid activation function
-pub fn sigmoid(x: &Array2<f64>) -> Array2<f64> 
-{
-    x.mapv(|v| 1.0 / (1.0 + (-v).exp()))
+pub trait Activation {
+    fn forward(&self, z: &Array2<f64>) -> Array2<f64>;
+    fn gradient(&self, z: &Array2<f64>) -> Array2<f64>;
 }
 
+/// ---------------------------
+/// SIGMOID
+/// ---------------------------
+pub struct Sigmoid;
+
+impl Activation for Sigmoid {
+    fn forward(&self, z: &Array2<f64>) -> Array2<f64> {
+        z.mapv(|v| 1.0 / (1.0 + (-v).exp()))
+    }
+
+    fn gradient(&self, z: &Array2<f64>) -> Array2<f64> {
+        let a = self.forward(z);
+        &a * &(1.0 - &a)
+    }
+}
